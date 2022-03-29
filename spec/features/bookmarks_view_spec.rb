99e34a9cc8cabe2961a8b6e139a73a14701ae2ee
Lucinda Curnow <lucinda.curnow@gmail.com>
2022-03-29 14:50:30 +0100

    Refactored list feature test

--- spec/features/bookmarks_view_spec.rb
+++ spec/features/bookmarks_view_spec.rb
@@ -1,10 +1,8 @@
 feature 'Viewing bookmarks' do
   scenario 'shows bookmarks' do
-    connection = PG.connect(dbname: 'bookmark_manager_test')
-
-    connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
-    connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.facebook.com');")
-    connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.twitter.com');")
+    BookmarkList.add("http://www.google.com")
+    BookmarkList.add("http://www.facebook.com")
+    BookmarkList.add("http://www.twitter.com")
 
     visit ('/bookmarks')
     expect(page).to have_content 'http://www.google.com'
